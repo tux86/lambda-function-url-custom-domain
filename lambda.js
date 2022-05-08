b58cf795887fcf0cf2f2a46cc08e65583de0388c
Walid Karray <walid.karray@gmail.com>
2022-05-08 02:18:51 +0200

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda Function Url !'),
+        body: JSON.stringify('Hello from Lambda Function URLs !'),
     };
 };
